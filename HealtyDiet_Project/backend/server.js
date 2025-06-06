0572cbb18ceba6f86a84eccb4fe528b320e05c13
Ghost <ghost200802@163.com>
2025-06-06 20:10:21 +0800

    remove data service

--- HealtyDiet_Project/backend/server.js
+++ HealtyDiet_Project/backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { ensureDataFilesExist } = require('./services/dataService');
+const { ensureDataFilesExist } = require('./services/index');
 
 // 初始化Express应用
 const app = express();
